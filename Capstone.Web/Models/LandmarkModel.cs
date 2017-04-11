6008b3c6a22257de58fcabe6ec0c4f426921c172
Jason McCoy <siaslmc@gmail.com>
2017-04-11 10:41:41 -0400

    Added landmark model

---
+++ Capstone.Web/Models/LandmarkModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class LandmarkModel
+    {
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public string description { get; set; }
+
+        public string image { get; set; }
+
+        public bool approved { get; set; }
+
+    }
+}
