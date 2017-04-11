a5c78df440abb8febad83a39de474afc3b725c54
Amanda Ronis <amanda.ronis@gmail.com>
2017-04-11 10:24:09 -0400

    Start of Itinerary work

--- Capstone.Web/DataAccess/ItinerarySQLDAL.cs
+++ Capstone.Web/DataAccess/ItinerarySQLDAL.cs
@@ -7,6 +7,24 @@ namespace Capstone.Web.DataAccess
 {
     public class ItinerarySQLDAL
     {
-        private string SQL_InsertNewItinerary = ""
-    }
+        private string SQL_InsertNewItinerary = "INSERT INTO itinerary VALUES (@name, @user_id, @starting_point);";
+        private string SQL_DeleteItinerary = "DELETE FROM itinerary WHERE itinerary_id = @itinerary_id;";
+        private string SQL_GetAllItineraries = "SELECT name FROM itinerary WHERE user_id = @user_id;";
+
+        public bool InsertNewItinerary(Itinerary itinerary)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString))
+                {
+                    conn.Open();
+                    int rowsAffected = db.Execute(SQL_InsertNewItinerary, itinerary);
+                    return (rowsAffected > 0);
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
 }
