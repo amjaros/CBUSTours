a023b2ff60106b89d021899219ec0fc37cfe17c9
Joe Osterfeld <joeosterfeld@gmail.com>
2017-04-11 14:57:15 -0400

    Added LandmarkDetail and ItineraryDetail models. Added to the dashboard controller.

--- Capstone.Web/Controllers/DashboardController.cs
+++ Capstone.Web/Controllers/DashboardController.cs
@@ -4,22 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Capstone.Web.Models;
+using Capstone.Web.DataAccess;
 
 namespace Capstone.Web.Controllers
 {
     public class DashboardController : Controller
     {
         // GET: Dashboard
+        [HttpGet]
         public ActionResult Dashboard(DashboardModel model)
         {
             return View("Dashboard", model);
         }
 
-        public ActionResult Itinerary()
+        [HttpGet]
+        public ActionResult ItineraryDetail(int id)
         {
+            List<LandmarkModel> landmarks = new LandmarkModel().SelectLandmarksByItinerary(id.ToString());
+
             ItineraryModel model = new ItineraryModel();
+            model.Landmarks = landmarks;
 
             return View("Itinerary", model);
+        }
+
+        [HttpGet]
+        public ActionResult LandmarkDetail(LandmarkModel model)
+        {
+            return View("LandmarkDetail", model);
         }
     }
 }
