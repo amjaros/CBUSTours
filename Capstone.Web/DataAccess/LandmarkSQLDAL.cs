6a5ca3de7975a6447d2ae2fea843d24330133e05
Andrew Jaros <amjaros1@gmail.com>
2017-04-12 12:28:48 -0400

    updates

--- Capstone.Web/DataAccess/LandmarkSQLDAL.cs
+++ Capstone.Web/DataAccess/LandmarkSQLDAL.cs
@@ -8,10 +8,6 @@ using Capstone.Web.Models;
 
 namespace Capstone.Web.DataAccess
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 4854e1024d9e7c79c751feb5708d24c1ffd8a7cb
     public class LandmarkSQLDAL
     {
         private string SQL_InsertLandmarkIntoItinerary = "INSERT INTO landmarks_by_itinerary VALUES (@itineraryID, @landmarkID)";
