817cbbb7ed3900bfbd54edc348673b9b194e62b6
Amanda Ronis <amanda.ronis@gmail.com>
2017-04-17 09:08:01 -0400

    Search Landmark Actions

--- Capstone.Web/Controllers/LandmarksController.cs
+++ Capstone.Web/Controllers/LandmarksController.cs
@@ -11,11 +11,23 @@ namespace Capstone.Web.Controllers
     public class LandmarksController : Controller
     {
         // GET: Landmarks
-        public ActionResult SearchLandmarks(int? id)
+        public ActionResult SearchLandmarks()
         {
             LandmarkSQLDAL DAL = new LandmarkSQLDAL();
             List<LandmarkModel> ApprovedLandmarks = DAL.GetApprovedLandmarks();
-            return View("SearchLandmarks", ApprovedLandmarks);
+            ItineraryModel itinModel = new ItineraryModel();
+            itinModel.Landmarks = ApprovedLandmarks;
+            return View("SearchLandmarks", itinModel);
+        }
+
+        public ActionResult SearchLandmarksWithItin(int id)
+        {
+            LandmarkSQLDAL DAL = new LandmarkSQLDAL();
+            List<LandmarkModel> ApprovedLandmarks = DAL.GetApprovedLandmarks();
+            ItineraryModel itinModel = new ItineraryModel();
+            itinModel.User_Id = id;
+            itinModel.Landmarks = ApprovedLandmarks;
+            return View("SearchLandmarks", itinModel);
         }
 
         public ActionResult LandmarkDetails(string landmarkID)
